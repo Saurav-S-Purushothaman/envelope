171deacae53dd25ebfb07cd1415cf69df4249772
Saurav S Purushothaman <saurav.purushothaman@unifize.com>
2024-09-07 23:32:22 +0530

    Create session and properties

--- src/envelope/core.clj
+++ src/envelope/core.clj
@@ -23,3 +23,27 @@
    [jakarta.activation
     DataHandler])
   (:gen-class))
+
+
+(defn properties [username host port ssl]
+  (let [properties (Properties.)]
+    (doto properties
+      (.put "mail.smtp.host" host)
+      (.put "mail.smtp.user" username)
+      (.put "mail.smtp.port" port)
+      (.put "mail.smtp.socketFactory.port" port)
+      (.put "mail.smtp.auth" "true"))
+    (when ssl
+      (doto properties
+        (.put "mail.smtp.starttls.enable" "true")
+        (.put "mail.smtp.socketFactory.class" "javax.net.ssl.SSLSocketFactory")
+        (.put "mail.smtp.socketFactory.fallback" "false")))
+    properties))
+
+
+(defn create-session!
+  [properties username password]
+  (Session/getInstance properties
+                       (proxy [Authenticator] []
+                         (getPasswordAuthentication []
+                           (PasswordAuthentication. username password)))))
